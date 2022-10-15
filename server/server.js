5c25a253489476cfba90d66583c6b4bbcaf420cf
CodeNinja-tech <elo48858@gmail.com>
2022-10-15 20:49:59 +0100

    Used middleware to change the default error handler

--- server/server.js
+++ server/server.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
-const port = process.env.PORT || 5000
+const { error_handler } = require('./middleware/error.middleware')
 
+const port = process.env.PORT || 5000
 const app = express()
 
 app.use(express.json())
@@ -12,6 +13,7 @@ app.get('/', (req, res) => {
 })
 
 app.use('/api/goals', require('./routes/goal.routes'))
+app.use(error_handler)
 
 app.listen(port, () => {
 	console.log(`Server started on port ${port}`)
