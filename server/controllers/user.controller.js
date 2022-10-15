06daddb7a75e0d93f6ba4ad3e499060a7f89ce93
CodeNinja-tech <elo48858@gmail.com>
2022-10-16 00:38:31 +0100

    Created Register and Login Functions

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,3 +1,8 @@
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs')
+const asyncHandler = require('express-async-handler')
+const User = require('../models/user.model')
+
 /**
  * @desc Create **a new user**
  * @route **POST** /api/users
@@ -5,9 +10,43 @@
  * @param {*} req
  * @param {*} res
  */
-const register_user = (req, res) => {
-  res.json({ msg: 'Register User' })
-}
+const register_user = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body
+  if (!name || !email || !password) {
+    res.status(400)
+    throw new Error('Please fill all fields')
+  }
+
+  // Check if user exists
+  const userExists = await User.findOne({ email })
+
+  if (userExists) {
+    res.status(400)
+    throw new Error('User already exists')
+  }
+
+  // Hash the password
+  const salt = await bcrypt.genSalt(10)
+  const hashedPassword = await bcrypt.hash(password, salt)
+
+  // Create user
+  const user = await User.create({
+    name,
+    email,
+    password: hashedPassword,
+  })
+
+  if (user) {
+    res.status(201).json({
+      _id: user.id,
+      name: user.name,
+      email: user.email,
+    })
+  } else {
+    res.status(400)
+    throw new Error('Invalid user data')
+  }
+})
 
 /**
  * @desc Authenticate **a user**
@@ -16,9 +55,24 @@ const register_user = (req, res) => {
  * @param {*} req
  * @param {*} res
  */
-const login_user = (req, res) => {
+const login_user = asyncHandler(async (req, res) => {
+  const { email, password } = req.body
+  const user = await User.findOne({ email })
+  if (!user) {
+    res.status(400)
+    throw new Error('user does not exist')
+  } else if (user && (await bcrypt.compare(password, user.password))) {
+    res.json({
+      _id: user.id,
+      name: user.name,
+      email: user.email,
+    })
+  } else {
+    res.status(400)
+    throw new Error('Invalid login data')
+  }
   res.json({ msg: 'Authenticate User' })
-}
+})
 
 /**
  * @desc Get **user data**
@@ -27,12 +81,12 @@ const login_user = (req, res) => {
  * @param {*} req
  * @param {*} res
  */
-const get_user_data = (req, res) => {
+const get_user_data = asyncHandler(async (req, res) => {
   res.json({ msg: 'User data' })
-}
+})
 
 module.exports = {
-	register_user,
-	login_user,
-	get_user_data
+  register_user,
+  login_user,
+  get_user_data,
 }
