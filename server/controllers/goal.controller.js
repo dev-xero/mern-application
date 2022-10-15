be5e02c773d5a0012137a476470843939a63e18e
CodeNinja-tech <elo48858@gmail.com>
2022-10-15 21:13:16 +0100

    Started using async-handler functions

--- server/controllers/goal.controller.js
+++ server/controllers/goal.controller.js
@@ -1,5 +1,3 @@
-const asyncHandler = require('express-async-handler')
-
 /**
  * @desc Get **all user** goals
  * @route **GET** /api/goals
@@ -7,9 +5,9 @@ const asyncHandler = require('express-async-handler')
  * @param {*} req 
  * @param {*} res 
  */
-const get_goals = asyncHandler(async (req, res) => {
-  res.status(200).json({ msg: 'GET Goals' })
-})
+const get_goals = (req, res) => {
+	res.status(200).json({ msg: 'GET Goals' })
+}
 
 /**
  * @desc Create **a new user** goal
@@ -18,14 +16,14 @@ const get_goals = asyncHandler(async (req, res) => {
  * @param {*} req 
  * @param {*} res 
  */
-const post_goals = asyncHandler(async (req, res) => {
-  const { text } = req.body
-  if (!text) {
-    res.status(400)
-    throw new Error('Please add a text field')
-  }
-  res.status(200).json({ msg: 'POST Goals' })
-})
+const post_goals = (req, res) => {
+	const { text } = req.body
+	if (!text) {
+		res.status(400)
+		throw new Error('Please add a text field')
+	}
+	res.status(200).json({ msg: 'POST Goals' })
+}
 
 /**
  * @desc Update **an existing user** goal
@@ -34,10 +32,10 @@ const post_goals = asyncHandler(async (req, res) => {
  * @param {*} req 
  * @param {*} res 
  */
-const put_goals = asyncHandler(async (req, res) => {
-  const { id } = req.params
+const put_goals = (req, res) => {
+	const { id } = req.params
   res.status(200).json({ msg: `UPDATE Goals ${id}` })
-})
+}
 
 /**
  * @desc Delete **a specific user** goal
@@ -46,10 +44,10 @@ const put_goals = asyncHandler(async (req, res) => {
  * @param {*} req 
  * @param {*} res 
  */
-const delete_goals = asyncHandler(async (req, res) => {
-  const { id } = req.params
+const delete_goals = (req, res) => {
+	const { id } = req.params
   res.status(200).json({ msg: `Delete Goals ${id}` })
-})
+}
 
 module.exports = {
   get_goals,
