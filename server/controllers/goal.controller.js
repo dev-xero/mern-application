920a907f191776b17fda447e5f7e3fe2df8f2231
CodeNinja-tech <elo48858@gmail.com>
2022-10-15 20:34:39 +0100

    Added Descriptive Comments Above Controller Functions

--- server/controllers/goal.controller.js
+++ server/controllers/goal.controller.js
@@ -1,16 +1,40 @@
+/**
+ * @desc Get **all user** goals
+ * @route **GET** /api/goals
+ * @param {*} req 
+ * @param {*} res 
+ */
 const get_goals = (req, res) => {
 	res.status(200).json({ msg: 'GET Goals' })
 }
 
+/**
+ * @desc Create **a new user** goal
+ * @route **POST** /api/goals
+ * @param {*} req 
+ * @param {*} res 
+ */
 const post_goals = (req, res) => {
 	res.status(200).json({ msg: 'POST Goals' })
 }
 
+/**
+ * @desc Update **an existing user** goal
+ * @route **PUT** /api/goals/:id
+ * @param {*} req 
+ * @param {*} res 
+ */
 const put_goals = (req, res) => {
 	const { id } = req.params
   res.status(200).json({ msg: `UPDATE Goals ${id}` })
 }
 
+/**
+ * @desc Delete **a specific user** goal
+ * @route **DELETE** /api/goals/:id
+ * @param {*} req 
+ * @param {*} res 
+ */
 const delete_goals = (req, res) => {
 	const { id } = req.params
   res.status(200).json({ msg: `Delete Goals ${id}` })
