d17cb5129375841ae76df895ef3c202f5ef31eb5
CodeNinja-tech <elo48858@gmail.com>
2022-10-16 15:16:12 +0100

    Updated Submit Button

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,9 +1,94 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { FaUser, FaChevronRight } from 'react-icons/fa'
 
 const Register = () => {
-	return (
-		<div>Register</div>
-	)
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+  })
+
+  const { name, email, password, confirmPassword } = formData
+
+  const handleChange = () => {}
+
+  return (
+    <>
+      <section className="heading">
+        <h1>
+          <FaUser />
+          &nbsp; Create Your Account
+        </h1>
+        <p>Create an account to continue</p>
+      </section>
+      <section className="form">
+        <form>
+          <div className="form-group">
+            {/* Username */}
+            <input
+              type="text"
+              className="form-control"
+              id="name"
+              name="name"
+              autoComplete="off"
+              autoCorrect="off"
+              placeholder="Your username"
+              onChange={handleChange}
+              value={name}
+            />
+          </div>
+          <div className="form-group">
+            {/* Email */}
+            <input
+              type="email"
+              className="form-control"
+              id="email"
+              name="email"
+              autoComplete="off"
+              autoCorrect="off"
+              placeholder="Your email address"
+              onChange={handleChange}
+              value={email}
+            />
+          </div>
+          <div className="form-group">
+            {/* Password */}
+            <input
+              type="password"
+              className="form-control"
+              id="password"
+              name="password"
+              autoComplete="off"
+              autoCorrect="off"
+              placeholder="Your password"
+              onChange={handleChange}
+              value={password}
+            />
+          </div>
+          <div className="form-group">
+            {/* Confirm Password */}
+            <input
+              type="password"
+              className="form-control"
+              id="confirm-password"
+              name="confirm-password"
+              autoComplete="off"
+              autoCorrect="off"
+              placeholder="Confirm your password"
+              onChange={handleChange}
+              value={confirmPassword}
+            />
+          </div>
+          <div className="form-group">
+            <button type="submit" className="btn btn-block">
+              SIGN UP <FaChevronRight />
+            </button>
+          </div>
+        </form>
+      </section>
+    </>
+  )
 }
 
 export default Register
